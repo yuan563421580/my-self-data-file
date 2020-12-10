d83978ed8e52f67fa224c29dcec2d60954e5fe93
yuansb <yuansb@asiainfo.com>
2020-12-10 10:15:24 +0800

    first commit

---
+++ src/main/java/com/yuansb/my/self/data/file/MySelfDataFileApplication.java
@@ -0,0 +1,13 @@
+package com.yuansb.my.self.data.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySelfDataFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySelfDataFileApplication.class, args);
+    }
+
+}
